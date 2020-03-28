6343811f3d9bcd961169e9b282a96b0cf536ddb7
sprice134 <46632787+sprice134@users.noreply.github.com>
2020-03-27 17:45:13 -0700

    figures out where parenthases are

--- balancer.py
+++ balancer.py
@@ -1,6 +1,5 @@
 def splitAlpha(x):
-    y = list()
-    print(len(x))
+    y = list()#y is a list of capital seperated components, splicing a string with a name was goiing to be way to complicated
     for i in range(len(x)):
         if x[i].isupper():
             y.append(i)
@@ -13,12 +12,33 @@ def splitAlpha(x):
                 sections.append(x[y[i]:y[i+1]])
             else:
                 sections.append(x[y[-1]:])
-        print(sections)
+        return sections
 
+def findSyntax(x):
+    y = int()
+    z = int()
+    count = 0
+    a = list()
+    for i in range(len(x)):
+        if (x[i].isalpha() == False):
+            if x[i] == "(":
+                y = i
+                count += 1
+            elif x[i] == ")":
+                z = i
+                count -= 1
+    if count != 0:
+        print("Please make sure you have the right syntax")
+        return []
+    a.append(y+1)
+    a.append(z)
+    return a
 
 def mainMethod():
-    print("Welcome")
+    print("Welcome, DISCLAIMER: Can only handle 1 set of parenthases per molecule")
     x = input("What is the first reactant: \n")
-    splitAlpha(x)
+    test = findSyntax(x)
+    if len(test) > 0:
+        print(x[test[0]:test[1]])
 
 mainMethod()
