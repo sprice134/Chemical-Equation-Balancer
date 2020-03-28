43751d1d216dadd8bab0ae6f3528d863a3663441
sprice134 <46632787+sprice134@users.noreply.github.com>
2020-03-27 17:22:36 -0700

    Switching to methods

--- balancer.py
+++ balancer.py
@@ -1,17 +1,24 @@
-print("Welcome")
-x = input("What is the first reactant: \n")
-y = list()
-print(len(x))
-for i in range(len(x)):
-    if x[i].isupper():
-        y.append(i)
-if len(y) == 1:
-    print(x)
-elif len(y) > 1:
-    sections = list()
-    for i in range(len(y)):
-        if i < len(y) -1:
-            sections.append(x[y[i]:y[i+1]])
-        else:
-            sections.append(x[y[-1]:])
-    print(sections)
+def splitAlpha(x):
+    y = list()
+    print(len(x))
+    for i in range(len(x)):
+        if x[i].isupper():
+            y.append(i)
+    if len(y) == 1:
+        print(x)
+    elif len(y) > 1:
+        sections = list()
+        for i in range(len(y)):
+            if i < len(y) -1:
+                sections.append(x[y[i]:y[i+1]])
+            else:
+                sections.append(x[y[-1]:])
+        print(sections)
+
+
+def mainMethod():
+    print("Welcome")
+    x = input("What is the first reactant: \n")
+    splitAlpha(x)
+
+mainMethod()
